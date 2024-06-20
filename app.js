29abe150874343c7a14c40c281881c8307be769f
Elliott <ellbutt@gmail.com>
2024-06-20 11:21:10 -0230

    project init

---
+++ app.js
@@ -0,0 +1,14 @@
+global.DEBUG = true;
+
+const fs = require("fs");
+
+const args = process.argv.slice(2);
+
+switch (args[0]) {
+  case "init":
+    break;
+  case "--help":
+  case "--h":
+  default:
+    break;
+}
