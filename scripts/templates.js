ff07ca77581af30b01fb9fb07b8708588a8c6ee3
Elliott <ellbutt@gmail.com>
2024-06-24 15:57:31 -0230

    changed 'myapp' to 'app' in help files

--- scripts/templates.js
+++ scripts/templates.js
@@ -5,8 +5,8 @@ const helpFiles = ["help-init.txt", "help-config.txt", "help-token.txt"];
 const configjson = {
   name: "AppConfigCLI",
   version: "1.0.0",
-  description: "The Command Line Interface (CLI) for the MyApp.",
-  main: "myapp.js",
+  description: "The Command Line Interface (CLI) for the app.",
+  main: "app.js",
   superuser: "adm1n",
   database: "exampledb",
 };
@@ -23,37 +23,37 @@ const tokenjson = [
   },
 ];
 
-const initHelp = `myapp init <option>
+const initHelp = `app init <option>
 
 Usage:
 
-myapp init --status                     displays status of all directories and files created during init 
-myapp init --all                        creates the folder structure and the config and help files
-myapp init --mk                         creates the folder structure
-myapp init --cat                        creates the config file with default settings and the help files`;
+app init --status                     displays status of all directories and files created during init 
+app init --all                        creates the folder structure and the config and help files
+app init --mk                         creates the folder structure
+app init --cat                        creates the config file with default settings and the help files`;
 
-const configHelp = `myapp config <option>
-
-Usage:
-
-myapp config --show                     displays a list of the current config settings
-myapp config --reset                    rests the config file with default settings
-myapp config --set <option> <value>     sets a specific config settings
-myapp config --add <attribute>          adds a new attribute to the config settings`;
-
-const tokenHelp = `myapp token <option>
+const configHelp = `app config <option>
 
 Usage:
 
-myapp token --count                       displays a count of the tokens created                      
-myapp token --check <username>            displays whether or not the specified token has expired                      
-myapp token --new <username>              generates a token for a given username, saves, tokens to the json file
-myapp token --add <username> <attribute>  adds a new attribute to the config settings
-myapp token --upd p <username> <phone>    updates the json entry with a new phone number and refreshes expiry if expired
-myapp token --upd e <username> <email>    updates the json entry with the new email and refreshes expiry if expired
-myapp token --search u <username>         fetches a token for a given username                         
-myapp token --search e <email>            fetches a token for a given email
-myapp token --search p <phone>            fetches a token for a given phone number`;
+app config --show                     displays a list of the current config settings
+app config --reset                    rests the config file with default settings
+app config --set <option> <value>     sets a specific config settings
+app config --add <attribute>          adds a new attribute to the config settings`;
+
+const tokenHelp = `app token <option>
+
+Usage:
+
+app token --count                       displays a count of the tokens created                      
+app token --check <username>            displays whether or not the specified token has expired                      
+app token --new <username>              generates a token for a given username, saves, tokens to the json file
+app token --add <username> <attribute>  adds a new attribute to the config settings
+app token --upd p <username> <phone>    updates the json entry with a new phone number and refreshes expiry if expired
+app token --upd e <username> <email>    updates the json entry with the new email and refreshes expiry if expired
+app token --search u <username>         fetches a token for a given username                         
+app token --search e <email>            fetches a token for a given email
+app token --search p <phone>            fetches a token for a given phone number`;
 
 module.exports = {
   folders,
